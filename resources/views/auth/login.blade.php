29678cad51faa9732dd168a1e7b175da12d4f837
2060375 <np03cs4s210135@heraldcollege.edu.np>
2022-05-28 23:50:30 +0545

    UI created

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,8 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+            <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            <h1 style="font-size:20px;font-weight:bold;">User Login</h1>
         </x-slot>
 
         <!-- Session Status -->
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,8 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+            <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            <h1 style="font-size:16px;font-weight:bold;">User Register</h1>
         </x-slot>
 
         <!-- Validation Errors -->
