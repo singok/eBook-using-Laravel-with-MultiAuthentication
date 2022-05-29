e52dc7241bed8ff633a7dbbd1935e7d98fb1b865
2060375 <np03cs4s210135@heraldcollege.edu.np>
2022-05-30 01:27:43 +0545

    UserDashboard file modified

--- app/Http/Livewire/UserDashboard.php
+++ app/Http/Livewire/UserDashboard.php
@@ -7,9 +7,19 @@ use Livewire\WithFileUploads;
 class UserDashboard extends Component
 {
     use WithFileUploads;
-    public $category, $title, $description, $file;
+    public $category, $title, $description, $book, $coverImage;
+
+    protected $rules = [
+        'category' => 'required',
+        'title' => 'required',
+        'description' => 'required',
+        'book' => 'required',
+        'coverImage' => 'image',
+    ];
+
     public function insert()
     {
+        $this->validate();
         dd($this->all());
     }
     public function render()
--- resources/views/livewire/user-dashboard.blade.php
+++ resources/views/livewire/user-dashboard.blade.php
@@ -3,74 +3,92 @@
      Dashboard
     @endsection
 
-    <button type="button" id="add-book-button" class="btn btn-primary"><i class="fa-solid fa-plus"></i>&nbsp;&nbsp;Add Book</button>
-    <!-- Table -->
-    <table class="table table-striped mt-4">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Book Title</th>
-            <th scope="col">Book Category</th>
-            <th scope="col">Short Description</th>
-            <th scope="col">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>
-                <i class="fa-solid fa-download"></i>
-                <i class="fa-solid fa-pen-to-square"></i>
-                <i class="fa-solid fa-trash"></i>
-            </td>
-          </tr>
-          
-        </tbody>
-      </table>
-
-      <div class="modal fade" id="book-add" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">New Book</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-                <div class="form-group">
-                    <label for="inputState">Category:</label>
-                    <select wire:model.defer="category" id="inputState" class="form-control">
-                      <option value="" selected>Choose...</option>
-                      <option value="Nobel">Nobel</option>
-                      <option value="Educational">Educational</option>
-                      <option value="Literature">Literature</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                  <label for="book-name" class="col-form-label">Title:</label>
-                  <input wire:model.defer="title" type="text" class="form-control" id="book-name">
-                </div>
-                <div class="form-group">
-                  <label for="description-text" class="col-form-label">Description:</label>
-                  <textarea wire:model.defer="description" class="form-control" id="description-text"></textarea>
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlFile1">Book (PDF):</label>
-                    <input wire:model.defer="file" type="file" class="form-control-file" id="exampleFormControlFile1">
-                </div>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button wire:click.prevent="insert" type="button" class="btn btn-primary">Add</button>
-            </div>
-          </div>
-        </div>
+    <div class="row mt-4">
+      <div class="col-md-8">
+          <!-- Table -->
+          <table class="table">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Book Title</th>
+                <th scope="col">Book Category</th>
+                <th scope="col">Short Description</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>
+                    <i class="fa-solid fa-download"></i>
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    <i class="fa-solid fa-trash"></i>
+                </td>
+              </tr>
+              
+            </tbody>
+          </table>
       </div>
-
+      <div class="col-md-4 border border-right-0 shadow p-3 mb-5 bg-white rounded">
+        <form wire:submit.prevent="insert" class="px-3 py-3">
+          <div class="form-group">
+            <label for="exampleFormControlSelect1">Book Category:</label>
+            <select wire:model="category" class="form-control" id="exampleFormControlSelect1">
+              <option value="" selected>Choose...</option>
+              <option value="nobel">Nobel</option>
+              <option value="educational">Educational</option>
+              <option value="comics">Comics</option>
+            </select>
+            @error('category')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+            @enderror
+          </div>
+          <div class="form-group">
+            <label for="book-title">Title:</label>
+            <input wire:model="title" type="text" class="form-control" id="book-title" placeholder="Enter book title">
+            @error('title')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+            @enderror
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlTextarea1">Short Description:</label>
+            <textarea wire:model="description" class="form-control" id="exampleFormControlTextarea1" rows="4" placeholder="Enter short description"></textarea>
+            @error('description')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+            @enderror
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlFile1">Cover Image:</label>
+            <input wire:model="coverImage" type="file" class="form-control-file" id="exampleFormControlFile1">
+            @error('coverImage')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+            @enderror
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlFile2">Book (PDF):</label>
+            <input wire:model="book" type="file" class="form-control-file" id="exampleFormControlFile2">
+            @error('book')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+            @enderror
+          </div>
+          <button type="submit" class="btn btn-primary"><i class="fa-solid fa-plus"></i>&nbsp;&nbsp;Add Book</button>
+        </form>
+      </div>
+    </div>
+    
     @push('script')
     <script>
         $(document).ready(function () {
